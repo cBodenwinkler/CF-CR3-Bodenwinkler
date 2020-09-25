ee51797a9d71a62b19de9914bd42e55cfe42aba3
Christian Bodenwinkler <c.bodenwinkler@gmx.at>
2020-09-25 18:38:34 +0200

    CSS mostly Done except for Main Input Part

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ function calculateInsurance() {
 	// Outer Comparison to check if Input was sufficient
 	if (inpName == "" || inpAge == 0 || inpHp == 0){
 		// Output when Input was NOT sufficient
-		document.getElementById("result").innerHTML = "No Input given";
+		document.getElementById("result").innerHTML = "Please Submit ALL the Data";
 	} else {
 		// inner Comparison for Calculation
 		if (inpCountry == "austria") {
